f927ab18f5500a4231f6819dfc0182782de8ecea
Mehebul02 <mehebul2123@gmail.com>
2026-01-12 13:17:24 +0600

    add the button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-
+import {Button} from "./components/ui";
 </script>
-
 <template>
 <router-view/>
+ <div class="flex min-h-screen justify-center items-center ">
+   <RouterLink to="/ui-design">
+     <Button variant="destructive" >Ui Design System</Button>
+   </RouterLink>
+ </div>
 </template>
